2199a021897a20ea8d0f256d1b9e1faa6a2ea2f6
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-02-13 11:22:27 +0000

    Merged PR 64: fb_Dashboard_Florian
    Progrès sur Dashboard

--- EasySave_G8_UI/MainWindow.xaml.cs
+++ EasySave_G8_UI/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using EasySave_G8_UI.View;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EasySave_G8_UI
 {
@@ -27,9 +15,29 @@ namespace EasySave_G8_UI
             Main.Content = new Dashboard();
         }
 
-        private void Classic_Save_Button(object sender, RoutedEventArgs e)
+        private void Classics_Click(object sender, RoutedEventArgs e)
         {
             Main.Content= new Classic_Save();
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Works_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Logs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         private void Main_Navigated(object sender, NavigationEventArgs e)
