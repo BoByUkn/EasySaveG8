b25ae627d13499c2e9b82de1997adb1f687d73c9
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-02-14 15:06:55 +0000

    Merged PR 76: fb_WorksInterface_Florian
    Corrections Forms

--- EasySave_G8_UI/Views/Logs.xaml.cs
+++ EasySave_G8_UI/Views/Logs.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using System;
 using System.Windows.Controls;
-//using System.Windows.Forms;
+using System.Windows.Forms;
 
 namespace EasySave_G8_UI.Views
 {
@@ -13,24 +13,24 @@ namespace EasySave_G8_UI.Views
         public Logs()
         {
             InitializeComponent();
-        }
+        }  
 
 
         private void ButtonLogs_Refresh(object sender, EventArgs e)
         {
-            //FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            FolderBrowserDialog folderbrowserdialog1 = new FolderBrowserDialog();
 
-            //if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-            //{
-            //    textBoxLogs.Text = "";
+            if (folderbrowserdialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxLogs.Text = "";
 
-            //    string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                string[] files = Directory.GetFiles(folderbrowserdialog1.SelectedPath);
 
-            //    foreach (string file in files)
-            //    {
-            //        textBoxLogs.Text += file + Environment.NewLine;
-            //    }
-            //}
+                foreach (string file in files)
+                {
+                    textBoxLogs.Text += file + Environment.NewLine;
+                }
+            }
         }
     }
 }
