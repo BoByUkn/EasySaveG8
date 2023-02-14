70ac54e15702a9d75e1243368aa7af4b9235eb82
Paolo Grilly <paolo.grilly@viacesi.fr>
2023-02-14 08:27:17 +0000

    Merged PR 70: fb_ClassicSavePage_Paolo
    - feat: add composant classic page
    - fix: fusion dev dans fb
    - feat; modif affichage
    - feat ajout background pour classic page
    - Fix: Grid background sur ClassicPage
    - fix: merge dev dans fb
    - feat: bouton launch Save
    
    Related work items: #13

--- EasySave_G8_UI/Views/Classics.xaml.cs
+++ EasySave_G8_UI/Views/Classics.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using EasySave_G8_UI.View_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace EasySave_G8_UI.Views
 {
@@ -10,6 +17,27 @@ namespace EasySave_G8_UI.Views
         public Classics()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = this.textBox1.Text;
+            string Source = this.textBox2.Text;
+            string Destination = this.textBox3.Text;
+            int indexType = this.comboBox1.SelectedIndex;
+            bool Type;
+            if (indexType == 0) { Type = true; }
+            else { Type = false; }
+
+            View_Model Classic_Save = new View_Model();
+            Classic_Save.VM_Classic(Name, Source, Destination, Type);
+
+
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
