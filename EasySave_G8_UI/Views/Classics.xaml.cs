bf03dedda8fd8df13adfb53cd2403210e6566285
Paolo Grilly <paolo.grilly@viacesi.fr>
2023-02-14 14:02:13 +0000

    Merged PR 72: fb_LogsPage_paolo
    - Feat: Ajout bouton Browse pour ajouter chemin Source et Destination dans Classic Save
    - fix: fix bug logs page
    - fix: log page
    
    Related work items: #14

--- EasySave_G8_UI/Views/Classics.xaml.cs
+++ EasySave_G8_UI/Views/Classics.xaml.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Drawing;
+using System.IO;
+using System.Windows.Threading;
+using Microsoft.Win32;
 
 namespace EasySave_G8_UI.Views
 {
@@ -38,6 +42,37 @@ namespace EasySave_G8_UI.Views
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
+        }
+
+        private void Button_Click_Browse(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.FileName = "Selectionner un dossier";
+            openFileDialog.Filter = "Dossiers|*.directory";
+            openFileDialog.InitialDirectory = @"C:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox2.Text = Path.GetDirectoryName(openFileDialog.FileName);
+            }
+        }
+        private void Button_Click_Browse2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.FileName = "Selectionner un dossier";
+            openFileDialog.Filter = "Dossiers|*.directory";
+            openFileDialog.InitialDirectory = @"C:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox3.Text = Path.GetDirectoryName(openFileDialog.FileName);
+            }
         }
     }
 }
--- EasySave_G8_UI/Views/Logs.xaml.cs
+++ EasySave_G8_UI/Views/Logs.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System;
+using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace EasySave_G8_UI.Views
 {
@@ -11,5 +14,25 @@ namespace EasySave_G8_UI.Views
         {
             InitializeComponent();
         }
+
+
+        private void ButtonLogs_Refresh(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxLogs.Text = "";
+
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+
+                foreach (string file in files)
+                {
+                    textBoxLogs.Text += file + Environment.NewLine;
+                }
+            }
+        }
+
+
     }
 }
