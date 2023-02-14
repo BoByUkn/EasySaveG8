c02fe78d9b9e3f1fc46789077caac675e286719a
Paolo Grilly <paolo.grilly@viacesi.fr>
2023-02-14 15:53:28 +0000

    Merged PR 77: fb_LogsPage_Paolo
    - Feat: Ajout bouton Browse pour ajouter chemin Source et Destination dans Classic Save
    - fix: fix bug logs page
    - fix: log page
    - feat: State log et Logs affichage sur la page
    - feat: ajout page stateLogs et Logs
    
    Related work items: #14

--- EasySave_G8_UI/Views/Logs.xaml.cs
+++ EasySave_G8_UI/Views/Logs.xaml.cs
@@ -32,5 +32,25 @@ namespace EasySave_G8_UI.Views
                 }
             }
         }
+
+
+        private void ButtonLogs_Refresh(object sender, EventArgs e)
+        {
+            textBoxLogs.Text = "";
+
+            string logsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EasySave", "logs");
+            string[] logFiles = Directory.GetFiles(logsFolder);
+
+            foreach (string logFile in logFiles)
+            {
+                textBoxLogs.Text += File.ReadAllText(logFile) + Environment.NewLine;
+            }
+        }
+        private void ButtonStateLogs_Refresh(object sender, EventArgs e)
+        {
+            textBoxLogs.Text = "";
+            textBoxLogs.Text = File.ReadAllText(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EasySave\StateLog.json");
+        }
     }
 }
+
