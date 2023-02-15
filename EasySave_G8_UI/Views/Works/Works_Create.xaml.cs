349bcd57658fda888d4f249fc3bdfa396f6c17dd
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-02-15 16:32:32 +0000

    Merged PR 89: fb_WorksInterface_Florian
    - Progrès sur interface Works
    - Progrès sur implémentation Works
    - Progrès sur Interface Works et corrections mineures
    - Merge branch 'develop' into fb_WorksInterface_Florian
    - Renommage CryptoSoft
    - Merge develop dans fb
    - Run Works fini
    - Delete Works et progrès sur Edit Works
    - Progrès sur Works Interface et corrections
    - Corrections traduction
    - Merge develop dans fb
    - Progrès sur création Works
    - Création Work OK

--- EasySave_G8_UI/Views/Works/Works_Create.xaml.cs
+++ EasySave_G8_UI/Views/Works/Works_Create.xaml.cs
@@ -19,6 +19,27 @@ namespace EasySave_G8_UI.Views.Works
         private void Save_btn_Click(object sender, RoutedEventArgs e)
         {
             View_Model ViewModel = new View_Model();
+            string Name = textBox1.Text;
+            if (ViewModel.VM_Work_Exist(Name))
+            {
+                LabelError.Content = "This name is already taken.";
+                return;
+            }
+
+            bool Type;
+            if (comboBox1.Text == "Complète" || comboBox1.Text == "Complete") { Type = true; }
+            else { Type = false; }
+
+            bool ExeNow;
+            if (comboBox2.Text == "Oui" || comboBox2.Text == "Yes") { ExeNow = true; } 
+            else { ExeNow = false; }
+
+            ViewModel.VM_Work_New(Name, textBox2.Text, textBox3.Text, Type, ExeNow);
+
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow.Main.Content = new Works();
+
+
         }
 
         private void Button_Click_Browse(object sender, RoutedEventArgs e)
