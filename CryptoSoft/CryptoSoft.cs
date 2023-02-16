57b71a5b9f02e8724de7d026f3628fd876413f1b
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-02-16 11:07:13 +0000

    Merged PR 97: v2.1
    - Ajout .gitignore, Dockerfile, .dockerignore et MAJ README.md
    - Ajout NewTonsoft
    - Ajout Modèles, Vue-Modèles et Asset depuis L1
    - Corrections pour adapter Modèles, Vue-Modèles et Assets
    - Merged PR 62: feat: add text box page EasySave classique
    - Merged PR 63: fb_Dashboard_Florian
    - Merged PR 64: fb_Dashboard_Florian
    - Deleted WpfApp1
    - Merged PR 65: fb_Dashboard_Florian
    - Merged PR 66: fb_DashBoard_Florian
    - Merged PR 67: fb_Dashboard_Florian
    - Merged PR 68: fb_Dashboard_Florian
    - Merged PR 70: fb_ClassicSavePage_Paolo
    - Merged PR 72: fb_LogsPage_paolo
    - Merged PR 74: fb_traduction_Matteo
    - Merged PR 75: fb_WorksInterface_Florian
    - Merged PR 76: fb_WorksInterface_Florian
    - Merged PR 77: fb_LogsPage_Paolo
    - fix : Button Fct en double dans logs
    - Merged PR 78: fb_blacklist_Matteo
    - Merged PR 79: fb_WorksInterface_Florian
    - Merged PR 81: fb_WorksInterface_Florian
    - Merged PR 83: fb_WorksInterface_Florian
    - Ajout de CryptoSoft (pas encore pleinement implémenté)
    - CryptoSoft
    - Merged PR 85: fb_WorksInterface_Florian
    - Merged PR 86: Fix: Clear des console write line
    - Merged PR 87: fb_WorksInterface_Florian
    - Merged PR 88: fb_WorksInterface_Florian
    - Fix : CryptoSoft
    - Fix :  Rebase
    - Fix : milliseconds for CryptoSoft
    - Merged PR 89: fb_WorksInterface_Florian
    - Merged PR 90: fb_blacklist_json_Matteo
    - Merged PR 91: fb_WorksInterface_Florian
    - Merged PR 92: fb_ClearModelAFT_LogsXML_Paolo
    - Merged PR 94: fb_traduction_update_Matteo
    - Merged PR 96: Merge CryptoSoft
    - Merged PR 93: fb_WorksInterface_Florian

---
+++ CryptoSoft/CryptoSoft.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+TimeSpan CryptoSoft(string Source, string Destination)
+{
+    using (var fin = new FileStream(Source, FileMode.Open))
+    using (var fout = new FileStream(Destination, FileMode.Create))
+    {
+        byte[] buffer = new byte[4096];
+        DateTime TimeStart = DateTime.Now; //Get starting time
+        while (true)
+        {
+            int bytesRead = fin.Read(buffer); //Read the file bit by bit
+            if (bytesRead == 0)
+                break;
+            EncryptBytes(buffer, bytesRead); //Encrypt the file bit by bit
+            fout.Write(buffer, 0, bytesRead);
+        }
+        DateTime TimeEnd = DateTime.Now; //Get finish time
+        TimeSpan Duration = TimeStart.Subtract(TimeEnd); //Get the duration of the encrypting
+        double msDuration = Duration.TotalMilliseconds; //and transform it in milliseconds
+
+        return Duration;
+    }
+}
+
+static long getKey(string filePath) //Will get the key by reading the file created by EasySave
+{
+    string inputString;
+
+    using (StreamReader reader = new StreamReader(filePath)) //Open the file for reading
+    {
+        inputString = reader.ReadToEnd(); //Read the entire contents of the file as a string
+    }
+
+    long key;
+    if (!long.TryParse(inputString, out key))
+    {
+        throw new ArgumentException("The input file contains invalid characters.");
+    }
+
+    return key;
+}
+long cipherKey = getKey(@"C:\Users" + Environment.UserName + @"\AppData\Roaming\EasySave\cipher key.txt");
+
+void EncryptBytes(byte[] buffer, int count)
+{
+
+    for (int i = 0; i < count; i++)
+        buffer[i] = (byte)(buffer[i] ^ cipherKey); //XOR operation bit by bit
+}
---
+++ EasySave_G8_UI/Views/Works/Works_Create.xaml.cs
@@ -0,0 +1,89 @@
+﻿using EasySave_G8_UI.View_Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using Microsoft.Win32;
+
+namespace EasySave_G8_UI.Views.Works
+{
+    /// <summary>
+    /// Logique d'interaction pour Works_Create.xaml
+    /// </summary>
+    public partial class Works_Create : Page
+    {
+        public Works_Create()
+        {
+            InitializeComponent();
+            translate();
+        }
+
+        private void translate()
+        {
+            CreateWork_Title.Text = $"{View_Model.VM_GetString_Language("create_work_title")}";
+            Name.Text = $"{View_Model.VM_GetString_Language("name")}";
+            Source_Path.Text = $"{View_Model.VM_GetString_Language("source_path")}";
+            Dest_Path.Text = $"{View_Model.VM_GetString_Language("dest_path")}";
+            Browse.Content = $"{View_Model.VM_GetString_Language("browse")}";
+            Browse2.Content = $"{View_Model.VM_GetString_Language("browse")}";
+            Save_btn.Content = $"{View_Model.VM_GetString_Language("save_work")}";
+            Execute_Now.Text = $"{View_Model.VM_GetString_Language("execute_now")}";
+        }
+
+        private void Save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            View_Model ViewModel = new View_Model();
+            string Name = textBox1.Text;
+            if (ViewModel.VM_Work_Exist(Name))
+            {
+                LabelError.Content = "This name is already taken.";
+                return;
+            }
+
+            bool Type;
+            if (comboBox1.Text == "Complète" || comboBox1.Text == "Complete") { Type = true; }
+            else { Type = false; }
+
+            bool ExeNow;
+            if (comboBox2.Text == "Oui" || comboBox2.Text == "Yes") { ExeNow = true; } 
+            else { ExeNow = false; }
+
+            ViewModel.VM_Work_New(Name, textBox2.Text, textBox3.Text, Type, ExeNow);
+
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow.Main.Content = new Works();
+
+
+        }
+
+        private void Button_Click_Browse(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.FileName = "Selectionner un dossier";
+            openFileDialog.Filter = "Dossiers|*.directory";
+            openFileDialog.InitialDirectory = @"C:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox2.Text = Path.GetDirectoryName(openFileDialog.FileName);
+            }
+        }
+        private void Button_Click_Browse2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.FileName = "Selectionner un dossier";
+            openFileDialog.Filter = "Dossiers|*.directory";
+            openFileDialog.InitialDirectory = @"C:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox3.Text = Path.GetDirectoryName(openFileDialog.FileName);
+            }
+        }
+    }
+}
