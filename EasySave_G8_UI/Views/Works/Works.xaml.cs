3f04b41679c03e2bf7200426ee9416e8f3d63bf2
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-02-16 15:36:52 +0100

    Fix Works next

--- EasySave_G8_UI/Views/Works/Works.xaml.cs
+++ EasySave_G8_UI/Views/Works/Works.xaml.cs
@@ -93,7 +93,49 @@ namespace EasySave_G8_UI.Views.Works
 
         private void Next_btn_Click(object sender, RoutedEventArgs e)
         {
+            if (List_Works.SelectedItems.Count == 0 || List_Works.SelectedItems.Count == 1) { return; }
 
+            string CurrentWork = "";
+            string WorkName = "";
+            bool AfterCurrent = false;
+            bool WorkNameFound = false;
+
+            foreach (string Item in List_Works.SelectedItems)
+            {
+                if (List_Work_Detail.Text.Contains(Item)) { CurrentWork = Item; }
+            }
+
+            while (!WorkNameFound)
+            {
+                foreach (string Item in List_Works.SelectedItems)
+                {
+                    if (Item == CurrentWork) { AfterCurrent = true; }
+                    if (Item != CurrentWork && AfterCurrent)
+                    {
+                        WorkName = Item;
+                        WorkNameFound = true;
+                    }
+                    else if (AfterCurrent)
+                    {
+                        WorkName = Item;
+                        WorkNameFound = true;
+                    }
+                }
+            }
+
+            List_Work_Detail.Text = "";
+
+            View_Model ViewModel = new View_Model();
+            List<Model_PRE> obj_list = ViewModel.VM_Work_Show(WorkName, false);
+
+            foreach (Model_PRE obj in obj_list)
+            {
+                List_Work_Detail.Text = "Name: " + obj.Name + "\n";
+                List_Work_Detail.Text += "Source: " + obj.Source + "\n";
+                List_Work_Detail.Text += "Destination: " + obj.Destination + "\n";
+                if (obj.Type) { List_Work_Detail.Text += "Type: Complete \n"; }
+                else { List_Work_Detail.Text += "Type: Differential \n"; }
+            }
         }
     }
 }
