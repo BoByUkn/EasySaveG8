d19bfbf403f0ccea52277ffae61292d46fc49df2
Paolo Grilly <paolo.grilly@viacesi.fr>
2023-02-17 12:37:03 +0000

    Merged PR 111: fb_XMLCorrections_AjoutButtonSwitch_Paolo
    - Final Fix develop
    - Corrections XML
    **- feat: Ajout d'un boutton pour switch affichage des logs de XML à JSON**

--- EasySave_G8_UI/Views/Logs.xaml.cs
+++ EasySave_G8_UI/Views/Logs.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Windows.Controls;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using EasySave_G8_UI.View_Models;
+using System.Windows.Forms;
 
 namespace EasySave_G8_UI.Views
 {
@@ -26,13 +27,13 @@ namespace EasySave_G8_UI.Views
 
         private void ButtonLogs_Refresh(object sender, EventArgs e)
         {
-            String folderPath = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EasySave\logs\";// Get the files in the folder
-            string[] files = Directory.GetFiles(folderPath);      
-            textBoxLogs.Clear();// Clear the contents of the text box
-            foreach (string file in files)// Loop through the files and add them to the text box
+            if (LogsButton.Content == "XML")
             {
-                textBoxLogs.AppendText(file + Environment.NewLine);
-                textBoxLogs.AppendText(File.ReadAllText(file) + Environment.NewLine);
+                ChangeLogs("XML");
+            }
+            else
+            {
+                ChangeLogs("JSON");
             }
         }
 
@@ -42,6 +43,33 @@ namespace EasySave_G8_UI.Views
             String file = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EasySave\StateLog.json";
             textBoxLogs.AppendText(file + Environment.NewLine);
             textBoxLogs.AppendText(File.ReadAllText(file) + Environment.NewLine);
+        }
+
+        private void LogsButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (LogsButton.Content == "XML")
+            {
+                LogsButton.Content = "JSON";
+                ChangeLogs("JSON");
+            }
+            else
+            {
+                LogsButton.Content = "XML";
+                ChangeLogs("XML");
+            }
+        }
+
+
+        private void ChangeLogs(string type)
+        {
+            String folderPath = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EasySave\logs\" + type + @"\" ;// Get the files in the folder
+            string[] files = Directory.GetFiles(folderPath);
+            textBoxLogs.Clear();// Clear the contents of the text box
+            foreach (string file in files)// Loop through the files and add them to the text box
+            {
+                textBoxLogs.AppendText(file + Environment.NewLine);
+                textBoxLogs.AppendText(File.ReadAllText(file) + Environment.NewLine);
+            }
         }
     }
 }
